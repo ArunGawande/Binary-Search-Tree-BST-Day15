eca0fb61d1801aab017888aa4b0e146a6eb8e3b8
Arun <arungawande1995@gmail.com>
2022-04-02 23:24:30 +0530

    Added: include Search operation on the BST

--- bst/MyBinaryTree.java
+++ bst/MyBinaryTree.java
@@ -4,17 +4,17 @@ public class MyBinaryTree<K extends Comparable<K>>
 {
     private MyBinaryNode<K> root;
 
-    private MyBinaryNode<K> addRecursively(MyBinaryNode<K> current, K key){
+    private MyBinaryNode<K> addRecursively(MyBinaryNode<K> current, K key) {
 
-        if(current == null) {
+        if (current == null) {
             return new MyBinaryNode<K>(key);
         }
 
         int compareResult = key.compareTo(current.key);
 
-        if(compareResult == 0)
+        if (compareResult == 0)
             return current;
-        if(compareResult < 0)
+        if (compareResult < 0)
             current.left = addRecursively(current.left, key);
         else
             current.right = addRecursively(current.right, key);
@@ -27,8 +27,23 @@ public class MyBinaryTree<K extends Comparable<K>>
         this.root = this.addRecursively(root, key);
     }
 
+    private boolean searchTreeRecursively(MyBinaryNode<K> current, K key) {
+        if (current == null) {
+            return false;
+        }
+        int compareResult = key.compareTo(current.key);
+        if (compareResult == 0) {
+            return true;
+        }
+        return compareResult < 0 ? searchTreeRecursively(current.left, key) : searchTreeRecursively(current.right, key);
+    }
+
+    public boolean searchTree(K key) {
+        return searchTreeRecursively(root, key);
+    }
+
     private int getSizeRecursively(MyBinaryNode<K> current) {
-        return current == null? 0: 1 + this.getSizeRecursively(current.left)+this.getSizeRecursively(current.right);
+        return current == null ? 0 : 1 + this.getSizeRecursively(current.left) + this.getSizeRecursively(current.right);
     }
 
     public int getSize() {
